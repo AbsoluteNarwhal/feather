3be67309d7ca6999075608891161878804f1bcae
Alexander Martin <alexandergomesmartin@gmail.com>
2025-04-27 20:16:35 +0100

    comment

--- sandbox/src/main.cpp
+++ sandbox/src/main.cpp
@@ -1,5 +1,7 @@
 #include "featherengine.h"
 
+// Note: in the future, the main function should be in the engine, not here. This is just a test to see if the engine works.
+
 class Sandbox : public ft::Application {
 public:
     const char *getClientName() const override {
